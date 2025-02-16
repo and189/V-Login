0ab0b1f5da6b558382281162577edadeda2bf99a
and189 <and1_89@live.de>
2025-02-16 15:20:01 +0000

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+// config/constants.js
+module.exports = {
+    IMPERVA_CHECK_TEXT: "Request unsuccessful. Incapsula",
+    LOGIN_URL: "https://access.pokemon.com/login",
+    BASE_AUTH_URL: "https://access.pokemon.com/oauth2/auth",
+    DEFAULT_TIMEOUT: 30000, // in milliseconds
+    HEADLESS_TIMEOUT: 30000, // in milliseconds - wird jetzt verwendet
+};
