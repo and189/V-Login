4c359e274e5f3cdb2fd863775d8e35c7c287ff20
and189 <and1_89@live.de>
2025-02-16 15:20:48 +0000

    Create auth_response.js

---
+++ core/auth_response.js
@@ -0,0 +1,9 @@
+// core/auth_response.js
+const AuthResponseStatus = {
+    SUCCESS: "SUCCESS",
+    INVALID: "INVALID",
+    BANNED: "BANNED",
+    ERROR: "ERROR"
+};
+
+module.exports = { AuthResponseStatus };
