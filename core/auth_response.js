f577b4ee41e767655c7b086b5d991f1ed1ce94f8
and189 <and1_89@live.de>
2025-02-20 01:15:43 +0000

    Update auth_response.js

--- core/auth_response.js
+++ core/auth_response.js
@@ -1,9 +1,13 @@
 // core/auth_response.js
+const logger = require('../utils/logger');
+
 const AuthResponseStatus = {
     SUCCESS: "SUCCESS",
     INVALID: "INVALID",
     BANNED: "BANNED",
     ERROR: "ERROR"
 };
+
+logger.debug(`auth_response module loaded with statuses: ${JSON.stringify(AuthResponseStatus)}`);
 
 module.exports = { AuthResponseStatus };
